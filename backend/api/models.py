bbaee1aaaead4f45e79c685b008a55217d4d2872
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-07-30 10:51:32 +0700

    refactor: change the product models slug, and image uploads handler

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,22 +1,21 @@
+import os
 from django.db import models
+from django.dispatch import receiver
 from slugify import slugify
 
 from django.contrib.auth.models import User
 
-def generate_product_slug(name):
-    slug = slugify(name)
-    product_count = Product.objects().filter(slug=slug).count()
-
-    if product_count == 0:
-        return slug
-    else:
-        new_slug = slugify(f"{slug}-{product_count}")
-        return new_slug
-
+def image_upload_path(instance, filename):
+    extension = filename.split('.')[-1]
+    filename_slug = slugify(instance.title)
+    new_filename = "product/images/%s_thumbnail.%s" % (filename_slug, extension)
+    
+    return new_filename
 
 class Product(models.Model):
-    slug = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(unique=True)
     title = models.CharField(max_length=250, default='')
+    image = models.ImageField(verbose_name='image', name="image", upload_to=image_upload_path, blank=True)
     archived = models.BooleanField(default=False)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,3 +23,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)
+        
+        if not self.slug:
+            self.slug = slugify(self.title) + "-" + str(self.id)
+            self.save()
+
+
+@receiver(models.signals.post_delete, sender=Product)
+def auto_delete_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+            
+
+@receiver(models.signals.pre_save, sender=Product)
+def auto_delete_image_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    
+    try:
+        old_file = Product.objects.get(pk=instance.pk).image
+    except Product.DoesNotExist:
+        return False
+    
+    new_file = instance.image
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
