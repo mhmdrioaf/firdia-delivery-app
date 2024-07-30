9b48a4f3dcf1e86c6a4f359ba2623129db77f5b7
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-07-30 12:22:59 +0700

    feat(frontend): add the create product page route

---
+++ frontend/src/app/seller/products/create/page.tsx
@@ -0,0 +1,9 @@
+import ProductForm from "@/components/forms/ProductForm";
+
+export default function ProductCreatePage() {
+  return (
+    <div className="flex min-h-svh w-full flex-col items-center justify-center gap-2 px-2 py-2 md:px-8">
+      <ProductForm />
+    </div>
+  );
+}
