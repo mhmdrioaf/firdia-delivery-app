8f39e77af411efd2065f90d7a054fdf8768500ef
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-07-30 12:23:41 +0700

    feat(frontend): implements the product card and product card skeleton components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Homepage() {
+import ProductCardSkeleton from "@/components/cards/ProductCardSkeleton";
+import { listAllProducts } from "@/lib/api/product";
+import { lazy, Suspense } from "react";
+
+const ProductCard = lazy(() => import("@/components/cards/ProductCard"));
+
+export default async function Homepage() {
+  const products = await listAllProducts();
   return (
-    <div className="w-screen px-8 py-2 min-h-[200svh] flex flex-col">
-      <p>Firdia Delivery App</p>
+    <div className="flex min-h-svh w-full flex-col gap-4 px-4 py-2 md:px-8">
+      <div className="flex w-full flex-col gap-2">
+        <b className="text-base md:text-lg">Produk produk yang tersedia</b>
+      </div>
+      <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <Suspense fallback={<ProductCardSkeleton />} key={product.id}>
+              <ProductCard key={product.id} product={product} />
+            </Suspense>
+          ))
+        ) : (
+          <i className="col-span-2 text-center text-xs md:col-span-3 md:text-sm lg:col-span-4">
+            Saat ini tidak ada produk yang tersedia...
+          </i>
+        )}
+      </div>
     </div>
   );
 }
--- frontend/src/app/seller/page.tsx
+++ frontend/src/app/seller/page.tsx
@@ -1,32 +1,33 @@
-import ProductDeleteForm from "@/components/forms/ProductDeleteForm";
+import ProductCard from "@/components/cards/ProductCard";
 import ProductForm from "@/components/forms/ProductForm";
+import { Button } from "@/components/ui/button";
 import { listProducts } from "@/lib/api/product";
+import Link from "next/link";
 
 export default async function SellerPage() {
   const products = await listProducts();
 
   if (products.length < 1) {
     return (
-      <div className="w-full min-h-svh flex flex-col gap-2 items-center justify-center px-2 md:px-8 py-2">
+      <div className="flex min-h-svh w-full flex-col items-center justify-center gap-2 px-2 py-2 md:px-8">
         <ProductForm firstTime />
       </div>
     );
   }
 
   return (
-    <div className="w-full min-h-svh grid grid-cols-2 md:grid-cols-3 gap-4 px-2 md:px-8 py-2">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="w-full h-full flex flex-col gap-2 rounded-md overflow-hidden border border-input relative"
-        >
-          <div className="absolute top-4 right-4">
-            <ProductDeleteForm productId={product.id} />
-          </div>
-          <b>{product.title}</b>
-          <p className="text-xs truncate">{product.description}</p>
-        </div>
-      ))}
+    <div className="flex min-h-svh w-full flex-col gap-4 px-4 py-2 md:px-8">
+      <div className="inline-flex w-full items-center justify-between">
+        <h3 className="text-2xl font-bold">Produk Anda</h3>
+        <Button asChild>
+          <Link href="/seller/products/create">Tambahkan Produk</Link>
+        </Button>
+      </div>
+      <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
