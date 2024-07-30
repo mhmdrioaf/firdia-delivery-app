f2e3b275d3a9a2f097cbb30c58cc924e1e0168cd
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-07-30 12:26:41 +0700

    refactor(frontend): change the form submit handler from server action to the default onSubmit function

--- frontend/src/components/forms/ProductDeleteForm.tsx
+++ frontend/src/components/forms/ProductDeleteForm.tsx
@@ -1,53 +1,44 @@
 "use client";
 
 import { deleteProduct } from "@/lib/api/product";
-import {
-  ProductApiStatus,
-  TProductDeleteResponse,
-} from "@/lib/api/product-type";
+import { ProductApiStatus } from "@/lib/api/product-type";
 import { useRouter } from "next/navigation";
 import React from "react";
-import { useFormState } from "react-dom";
 import { useToast } from "../ui/use-toast";
 import ProductDeleteButton from "./ProductDeleteButton";
-
-const initialFormState: TProductDeleteResponse = {
-  message: "",
-  status: ProductApiStatus.Idle,
-};
 
 export default function ProductDeleteForm({
   productId,
 }: {
   productId: number;
 }) {
-  const [state, formAction] = useFormState(
-    deleteProduct.bind(null, productId),
-    initialFormState
-  );
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   const router = useRouter();
   const { toast } = useToast();
 
-  React.useEffect(() => {
-    if (state.status !== ProductApiStatus.Idle) {
-      if (state.status === ProductApiStatus.Success) {
-        toast({
-          description: state.message,
-        });
-        router.refresh();
-      } else {
-        toast({
-          description: state.message,
-          variant: "destructive",
-        });
-      }
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const response = await deleteProduct(productId);
+
+    if (response.status === ProductApiStatus.Success) {
+      toast({
+        description: response.message,
+      });
+      router.refresh();
+    } else {
+      toast({
+        description: response.message,
+        variant: "destructive",
+      });
     }
-  }, [state, toast, router]);
+  };
 
   return (
-    <form action={formAction}>
-      <ProductDeleteButton />
+    <form onSubmit={handleSubmit}>
+      <ProductDeleteButton disabled={isLoading} />
     </form>
   );
 }
